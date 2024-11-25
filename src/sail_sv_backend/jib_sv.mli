4c89cbeb3321ef74b2893ea65d40a6ef91d8341a
Nicolas Phan <nicolas.phan@codasip.com>
2024-11-25 11:59:10 +0100

    [SV_backend] Add flag to replace unions with structs
    Some EDA tools don't allow union types where the fields have different widths.
    This typically happens on types like option = Some of a | None of unit
    where 'a' and 'unit' have different widths.
    
    There is already a flag to disabled packed unions and use unpacked
    structures instead, but EDA tools may not support unpacked typed as well.
    
    Thus, this flag ensures the ADTs only yield _packed structures_,
    which should be compatible with most tools.

--- src/sail_sv_backend/jib_sv.mli
+++ src/sail_sv_backend/jib_sv.mli
@@ -81,6 +81,7 @@ module type CONFIG = sig
 
   val never_pack_unions : bool
   val union_padding : bool
+  val nounions : bool
   val unreachable : string list
   val comb : bool
   val ignore : string list
