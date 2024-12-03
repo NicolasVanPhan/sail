8c253634f1da8881c8f976e1e2da11eafd2c6f8d
Nicolas Phan <nicolas.phan@codasip.com>
2024-12-03 18:11:00 +0100

    [SV_backend] Add flag to configure recursion depth
    Going from 10 to 2 significantly reduces the SV generation time.

--- src/sail_sv_backend/jib_sv.ml
+++ src/sail_sv_backend/jib_sv.ml
@@ -548,6 +548,7 @@ let collect_spec_info ctx cdefs =
   }
 
 module type CONFIG = sig
+  val recursion_depth : int
   val max_unknown_integer_width : int
   val max_unknown_bitvector_width : int
   val line_directives : bool
@@ -568,6 +569,7 @@ module Make (Config : CONFIG) = struct
   module Primops =
     Generate_primop2.Make
       (struct
+        let recursion_depth = Config.recursion_depth
         let max_unknown_bitvector_width = Config.max_unknown_bitvector_width
         let max_unknown_integer_width = Config.max_unknown_integer_width
       end)
@@ -2339,7 +2341,11 @@ module Make (Config : CONFIG) = struct
     }
 
   let rec pp_module m =
-    let params = if m.recursive then space ^^ string "#(parameter RECURSION_DEPTH = 10)" ^^ space else empty in
+    let params =
+      if m.recursive then
+        space ^^ string (Printf.sprintf "#(parameter RECURSION_DEPTH = %d)" Config.recursion_depth) ^^ space
+      else empty
+    in
     let ports =
       match (m.input_ports, m.output_ports) with
       | [], [] -> semi
